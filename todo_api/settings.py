b47361f076dd3c69fd33b0b63e1f3a77a76188db
SurajSuresh123 <117701384+SurajSuresh123@users.noreply.github.com>
2023-10-15 01:01:34 +0530

    Update settings.py

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'todo_api.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    
         'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get("POSTGRES_DATABASE"),
@@ -88,7 +88,7 @@ DATABASES = {
         'HOST':os.environ.get("POSTGRES_HOST"),
         'PORT':os.environ.get("DB_POST"),
     }
-    }
+    
 }
 
 
