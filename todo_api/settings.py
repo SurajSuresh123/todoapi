cb3b13e8f734245b8af8c7ed7dcadb85a02dbda8
SurajSuresh123 <117701384+SurajSuresh123@users.noreply.github.com>
2023-10-15 11:52:17 +0530

    Update settings.py 2

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'USER':os.environ.get("POSTGRES_USER"),
         'PASSWORD':os.environ.get("POSTGRES_PASSWORD"),
         'HOST':os.environ.get("POSTGRES_HOST"),
-        'PORT':os.environ.get("DB_POST"),
+        'PORT':os.environ.get("DB_PORT"),
     }
     
 }
